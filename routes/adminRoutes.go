9b2115d8c08ad3ca533a78a59075755b0bfce11d
kunxl-gg <tiwari.25@iitj.ac.in>
2023-10-09 20:06:58 +0530

    Added middleware for for user routes
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- routes/adminRoutes.go
+++ routes/adminRoutes.go
@@ -3,9 +3,11 @@ package routes
 // Define all the Admin Routes here
 import (
 	"github.com/kunxl-gg/Amrit-Career-Counsellor.git/controllers"
+	"github.com/kunxl-gg/Amrit-Career-Counsellor.git/middlewares"
 	"github.com/kunxl-gg/Amrit-Career-Counsellor.git/types"
 )
 
 func SetupRoutes() {
+	types.AdminRoutes.Use(middlewares.CheckIfAdmin)
 	types.AdminRoutes.GET("/", controllers.PingController)
 }

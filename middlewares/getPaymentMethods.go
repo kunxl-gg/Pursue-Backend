b722236105ce4b50024124bef611c3e5d744a549
kunxl-gg <tiwari.25@iitj.ac.in>
2023-10-26 21:05:23 +0530

    Added Middleware function for Payment Method
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- middlewares/getPaymentMethods.go
+++ middlewares/getPaymentMethods.go
@@ -1,11 +1,56 @@
 package middlewares
 
-import "os"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+// MIDDLWARE: GetPaymentMethods - Gets the list of Payment Methods
 func GetPaymentMethods() {
 	
 	// JustPay API URL and Merchant ID
-	apiURL := os.Getenv("JUSPAY_SERVER_URL")
+	juspayServerURL := os.Getenv("JUSPAY_SERVER_URL")
+	juspayAPIKey := os.Getenv("JUSPAY_API_KEY")
 	merchantID := os.Getenv("MERCHANT_ID")
-	
+
+	// Final URL for fetching Payment Methods
+	url := juspayServerURL + "/merchant" + merchantID + "/paymentmethods"
+	fmt.Println(url)
+
+	// Creating a New Request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+
+	// Adding MerchantID to request Header
+	req.Header.Add("x-merchantid", merchantID)
+	req.Header.Add("Content-Type", "application/json")
+
+	// Adding Basic Auth using the API Key
+	req.SetBasicAuth(juspayAPIKey, "")
+
+	// Creating a Client
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+
+	// Closing the Response Body
+	defer resp.Body.Close()
+
+	// Reading the Response Body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+
+	// Printing the Response
+	fmt.Println(string(body))
+
 }

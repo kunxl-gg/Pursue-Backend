b1211c2036b8430250a2c33f8ee7f29082dfbae0
kunxl-gg <tiwari.25@iitj.ac.in>
2023-11-24 12:10:31 +0530

    Minor changes to test workflow
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- controllers/pingController.go
+++ controllers/pingController.go
@@ -7,5 +7,5 @@ import (
 
 // PingController Function to check if the server is running
 func PingController(ctx *gin.Context) {
-	ctx.String(http.StatusOK, "Pursuse Says Hello World!")
+	ctx.String(http.StatusOK, "Pursuse Says Hello World! 1")
 }

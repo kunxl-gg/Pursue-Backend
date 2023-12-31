2e966322998bb5e6ab542ada96d27d206657a3c8
kunxl-gg <tiwari.25@iitj.ac.in>
2023-12-31 11:15:21 +0530

    Added firebase functions for both questions and repositories
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

---
+++ controllers/repositoryController.go
@@ -0,0 +1,48 @@
+package controllers
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/kunxl-gg/Amrit-Career-Counsellor.git/middlewares/chatbot"
+	"log"
+	"net/http"
+)
+
+func ReadRepositoryController(ctx *gin.Context) {
+	// Reading the parameter from the URl
+	id := ctx.Param("id")
+
+	// Making a call to the backend function
+	data, err := chatbot.ReadRepository(id)
+	if err != nil {
+		ctx.String(http.StatusInternalServerError, err.Error())
+	}
+
+	// Returning the final response as a string
+	ctx.JSON(
+		http.StatusOK,
+		data,
+	)
+}
+
+func WriteToRepository(ctx *gin.Context) {
+	// Reading the request body
+	var requestBody struct {
+		DatabaseTable     *string
+		Parameters        []string
+		CareerSuggestions []string
+	}
+
+	err := ctx.Bind(&requestBody)
+	if err != nil {
+		log.Fatal(err)
+		ctx.String(http.StatusInternalServerError, err.Error())
+	}
+
+	// Call the function
+	id, err := chatbot.AddUserChoicesInRepository(*requestBody.DatabaseTable, requestBody.Parameters, requestBody.CareerSuggestions)
+	fmt.Println(id, err)
+
+	// Returning the final response Id of the object created
+	ctx.String(http.StatusOK, id)
+}
